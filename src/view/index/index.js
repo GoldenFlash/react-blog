628631140960b1782a3177f082f6870d326ac31f
GoldenFlash <892458829@qq.com>
2019-01-12 13:28:40 +0800

    修改导航条

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,5 +1,8 @@
 import React, { Component} from "react"
 import './index.scss'
+import home_img from '../../assets/home.svg'
+import lingdang_img from '../../assets/lingdang.svg'
+import search_img from '../../assets/search.svg'
 export default class Index extends Component{
     constructor(props){
         super(props)
@@ -14,12 +17,18 @@ export default class Index extends Component{
                     <header>
                         <div className="titleBar">
                             <div className="titleBar_left">
-                                <span>博客</span>
+                                <img style={{ width: "25px", height: "25px", marginRight: "10px" }} src={home_img}></img>
+                                <span style={{fontSize:"20px"}}>博客</span>
                             </div>
                             <div className="titleBar_middle">
-                                <span>搜索</span>
+                                {/* <span>搜索</span> */}
+                                <input className="titleBar_input" type="text" placeholder="输入关键词搜索..."/>
+                                <div className="titleBar_search">
+                                    <img src={search_img} style={{ width: "20px", height: "20px"}}/>
+                                </div>
                             </div>
                             <div className="titleBar_right">
+                                <img src={lingdang_img} style={{ width: "20px", height: "20px", marginRight: "10px" }}/>
                                 <spam> 登录</spam>
                             </div>
                         </div>
