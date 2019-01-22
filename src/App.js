b0e20741ec9af3e9f9531cd026e54e999bbf6ef1
GoldenFlash <892458829@qq.com>
2019-01-22 10:02:02 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import {BrowserRouter as Router, Route,Switch ,withRouter} from "react-router-dom";
+
 import './App.css';
 import 'antd/dist/antd.css';
+
 import Index from './view/index/index.js'
+import Article from './view/article/article'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Index></Index>
+        <Router >
+          <Switch>
+              <Route exact  path="/" component={Index}></Route>
+              <Route path="/article" component={Article}/>
+          </Switch> 
+        </Router> 
       </div>
     );
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route,Switch} from "react-router-dom";
+
 import './index.css';
 import App from './App';
-import Article from './view/article/article'
+
 import * as serviceWorker from './serviceWorker';
 
-import createBrowserHistory from 'history/createBrowserHistory'
-const history = createBrowserHistory()
-ReactDOM.render(
-    <Router >
-        <div>
-            <Switch>
-                <Route exact  path="/" component={App}></Route>
-                <Route path="/article" component={Article}/>
-           
-            </Switch> 
-        </div>
-    </Router>  
-     
-    , document.getElementById('root'));
+
+ReactDOM.render(<App/>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
